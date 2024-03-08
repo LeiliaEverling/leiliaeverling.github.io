97c7ef48bad3e07856d9a1483069544d5198cfc3
Leilia Everling <155007519+LeiliaEverling@users.noreply.github.com>
2024-03-07 19:30:52 -0600

    Update storytree

--- 54236815/storyTree.js
+++ 54236815/storyTree.js
@@ -9,10 +9,62 @@ export default {
     "type": "Available",
     "children": [
         {
-            "name": "Hot Chocolate",
+            "name": "Shirogane's Phone",
             "work": 54236815,
             "chapter": 137345080,
-            "decision": "Access Acknowledgements Chapter",
+            "decision": "Access Shirogane's Phone",
+            "description": "Iino asks Ishigami if he wants to get hot chocolate after school.",
+            "rating": "General Audiences",
+            "warnings": "None",
+            "type": "Available",
+            "children": [
+                
+            ]
+        },
+        {
+            "name": "Shinomiya's Phone",
+            "work": 54236815,
+            "chapter": 137569645,
+            "decision": "Access Shinomiya's Phone",
+            "description": "Iino asks Ishigami if he wants to get hot chocolate after school.",
+            "rating": "General Audiences",
+            "warnings": "None",
+            "type": "Available",
+            "children": [
+                
+            ]
+        },
+        {
+            "name": "Fujiwara's Phone",
+            "work": 54236815,
+            "chapter": 137573308,
+            "decision": "Access Fujiwara's Phone",
+            "description": "Iino asks Ishigami if he wants to get hot chocolate after school.",
+            "rating": "General Audiences",
+            "warnings": "None",
+            "type": "Available",
+            "children": [
+                
+            ]
+        },
+        {
+            "name": "Ishigami's Phone",
+            "work": 54236815,
+            "chapter": 137573326,
+            "decision": "Access Ishigami's Phone",
+            "description": "Iino asks Ishigami if he wants to get hot chocolate after school.",
+            "rating": "General Audiences",
+            "warnings": "None",
+            "type": "Available",
+            "children": [
+                
+            ]
+        },
+        {
+            "name": "Iino's Phone",
+            "work": 54236815,
+            "chapter": 137573350,
+            "decision": "Access Iino's Phone",
             "description": "Iino asks Ishigami if he wants to get hot chocolate after school.",
             "rating": "General Audiences",
             "warnings": "None",
